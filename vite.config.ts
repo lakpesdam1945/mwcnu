122c1759cc27150d72994a6a338a355f2110e422
lakpesdam1945 <lakpesdam.rck@gmail.com>
2022-12-02 21:02:14 +0700

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,48 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      injectRegister: "auto",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      },
+      includeAssets: [
+        "icon-512x512.png",
+        "icon-256x256.png",
+        "icon-384x384.png",
+        "icon-192x192.png",
+      ],
+      manifest: {
+        name: "MWC RANCAEKEK",
+        short_name: "MyApp",
+        description: "Majelis Wakil Cabang Nahdlatul Ulama Kecamatan Rancaekek",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
